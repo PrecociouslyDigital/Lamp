1c919baee920599b2246bee54bef61008772367c
Sydney <Sydney@recroom.com>
2022-12-27 17:52:42 -0800

    pull out const

--- Entities/Gun Stuff/Trail.cs
+++ Entities/Gun Stuff/Trail.cs
@@ -6,11 +6,11 @@ public class Trail : Line2D
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
-
+    const float trailTime = 1/45;
     // Called when the node enters the scene tree for the first time.
     public override async void _Ready()
     {
-        await ToSignal(this.GetTree().CreateTimer(0.5f), "timeout");
+        await ToSignal(this.GetTree().CreateTimer(trailTime), "timeout");
         this.QueueFree();
     }
 
